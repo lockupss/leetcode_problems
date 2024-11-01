5dcac2a4a06ba61f15fc5c30a3880971721eb3dc
lockupss <166323020+lockupss@users.noreply.github.com>
2024-11-01 21:54:23 +0300

    Time: 0 ms (100%), Space: 52.8 MB (7.11%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let answer=[];
+    for (let i=1; i<=n; i++)
+    if(i%3==0 && i%5==0){
+     answer[i-1]="FizzBuzz";}
+     else if(i%3==0){
+      answer[i-1]="Fizz";}
+     else if(i%5==0){
+         answer[i-1]="Buzz";}
+     else{
+            answer[i-1]=String(i);
+        }
+        return answer;
+      };
+     
+     
+    
